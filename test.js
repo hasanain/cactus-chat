0d3d263d467c8a4ed74fffb52db82af4144bd94f
Sean Stephens <seanastephens@email.arizona.edu>
2015-07-18 15:57:19 -0700

    Updates

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 var tfchat = require('./tucson_fact_chat');
+var w = require('./weather_chat');
 
 tfhistory = {};
 var response = tfchat.respondTo('Hello', tfhistory);
 
 console.log(response);
 console.log(tfhistory);
+
+
+whistory = {};
+var response = w.respondTo('Hello', whistory);
+
+console.log(response);
+console.log(whistory);
+
