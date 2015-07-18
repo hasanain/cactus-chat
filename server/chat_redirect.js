a62c8c2a9a4c2ecd6d60ff33e424f66bd327fbd6
Sean Stephens <seanastephens@email.arizona.edu>
2015-07-18 16:28:59 -0700

    Go

--- server/chat_redirect.js
+++ server/chat_redirect.js
@@ -1,5 +1,6 @@
 module.exports.respondTo = function (catus_id, message, history) {
   var tucsonFactChatter = require('../tucson_fact_chat');
+  var weather = require('../weather_chat');
   
-  return tucsonFactChatter.respondTo(message, history);
+  return weather.respondTo(message, history);
 };
